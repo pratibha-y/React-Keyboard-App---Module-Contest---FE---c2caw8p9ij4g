511f290ca80284f798c4a2fdc6b579b3f1e23282
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-16 10:22:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import "../styles/App.css";
+/*import "../styles/App.css";
 import React, { useState, useEffect } from "react";
 
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
@@ -18,5 +18,50 @@ const App = () => {
     </div>
   );
 };
+
+export default App;*/
+
+import "../styles/App.css";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
+
+const App = () => {
+  const [preview, setPreview] = useState('');
+  const [quote, setQuote] = useState('');
+
+  const handleClick = (keyValue) => {
+    const updatedPreview = preview + keyValue;
+    setPreview(updatedPreview);
+
+    if (updatedPreview === 'forty two') {
+      axios.get('https://api.quotable.io/random').then((response) => {
+        setQuote(response.data.content);
+      });
+    } else {
+      setQuote('');
+    }
+  };
+  const renderKeys = () => {
+   
+    return keys.map((keyValue) => (
+      <button key={`key-${keyValue}`} id={`key-${keyValue === ' ' ? 'space' : keyValue}`} onClick={() => handleClick(keyValue)}>
+        {keyValue}
+      </button>
+    ));
+  };
+
+  return (
+    <div className="App">
+      <div className="preview">{preview}</div>
+      {preview === 'forty two' && <div className="quote">{quote}</div>}
+      <div className="keyboard">{renderKeys()}</div>
+    </div>
+  );
+
+};
+
+
 
 export default App;
